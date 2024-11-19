53173245221e032d59d10525c987d6ea9cd674c4
Egor Anakhin <eanahin8v@mail.ru>
2024-11-20 01:46:06 +0300

    feat: bot message (no options)

--- src/pages/chat/ui/bot.message.ui.tsx
+++ src/pages/chat/ui/bot.message.ui.tsx
@@ -13,6 +13,13 @@ export const BotMessage: FC<BotMessageProps> = ({ message, style }) => {
 			<div className='bot_avatar'>
 				<img src={BotAvatar} alt='Аватар бота' />
 			</div>
+			<div
+				style={{
+					marginTop: 7,
+				}}>
+				<div className='message_time'>{message.time}</div>
+				<div className='message_text bot_message_text'>{message.text}</div>
+			</div>
 		</div>
 	)
 }
