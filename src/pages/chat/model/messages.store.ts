cfdefb925f3235049dc57eb92c2874e573183025
Egor Anakhin <eanahin8v@mail.ru>
2024-11-24 08:34:27 +0300

    Chat mock

--- src/pages/chat/model/messages.store.ts
+++ src/pages/chat/model/messages.store.ts
@@ -6,25 +6,21 @@ export class MessagesStore {
 	private messages_: Message[] = [
 		{
 			sender: 'Bot',
-			text: `Добро пожаловать в чатбот! 
-Мы поможем вам составить бизнес-план,
-найти гранты или поддержку инвесторов.
-Введите ИНН.`,
-			time: '12:18',
-			options: [],
-		},
-		{
-			sender: 'User',
-			text: '7707083893',
-			time: '12:19',
-		},
-		{
-			sender: 'Bot',
-			text: 'Какой-то ответ, ля-ля-ля, пу-пу-пу',
-			options: ['Да', 'Нет'],
-			time: '12:20',
+			time: '12:15',
+			text:
+				'Добро пожаловать в чатбот! \n' +
+				'Мы поможем вам составить бизнес-план, найти гранты или поддержку инвесторов.\n' +
+				'\n' +
+				'Выберите свою категорию:',
+			options: ['Я только начинаю бизнес (стартаперы)', 'У меня уже есть бизнес (МСП)'],
 		},
 	]
+
+	private questionAnswerMap: { [key: string]: string } = {
+		'Какой ваш ИНН?': '7707083893',
+		'Какие гранты доступны?': 'Гранты доступны для стартапов и малого бизнеса.',
+		'Как найти инвесторов?': 'Мы поможем найти подходящих инвесторов для вашего проекта.',
+	}
 
 	constructor() {
 		makeAutoObservable(this)
@@ -44,6 +40,18 @@ export class MessagesStore {
 	public sendMessage(text: string) {
 		this.messages_.push({ sender: 'User', time: getCurrentTime(), text: text })
 		console.log(this.messages_.length)
+
+		const answer = this.questionAnswerMap[text]
+		if (answer) {
+			const delay = 500 + Math.random() * 200
+			setTimeout(() => {
+				this.messages_.push({
+					sender: 'Bot',
+					time: getCurrentTime(),
+					text: answer,
+				})
+			}, delay)
+		}
 	}
 }
 
